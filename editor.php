28bb5d2b3d18837cc37383339862a82eb78ffc32
Mohamad Supangat <hantamkoding@gmail.com>
2022-05-19 21:25:01 +0700

    change glob function to custom function

--- editor.php
+++ editor.php
@@ -2,22 +2,31 @@
 //include 'editor.html';
 $html = file_get_contents('editor.html');
 
+$dirs = ['demo', 'my-pages', 'projects', 'undangan'];
 //search for html files in demo and my-pages folders
-$htmlFiles = glob('{my-pages/*.html,demo/*\/*.html, demo/*.html}',  GLOB_BRACE);
-foreach ($htmlFiles as $file) { 
-   if (in_array($file, array('new-page-blank-template.html', 'editor.html'))) continue;//skip template files
-   $pathInfo = pathinfo($file);
-   $filename = $pathInfo['filename'];
-   $folder = preg_replace('@/.+?$@', '', $pathInfo['dirname']);
-   $subfolder = preg_replace('@^.+?/@', '', $pathInfo['dirname']);
-   if ($filename == 'index' && $subfolder) {
-	   $filename = $subfolder;
-   }
-   $url = $pathInfo['dirname'] . '/' . $pathInfo['basename'];
-   $name = ucfirst($filename);
+$htmlFiles = [];
+foreach ($dirs as $dir) {
+	$pattern = '';
+	for ($i = 0; $i < 6; $i++) {
+		$pattern .= '*/';
+		$htmlFiles = array_merge($htmlFiles, glob($dir . '/' . $pattern . '*.html'));
+	}
+}
 
-  $files .= "{name:'$name', file:'$filename', title:'$name',  url: '$url', folder:'$folder'},";
-} 
+foreach ($htmlFiles as $file) {
+	if (in_array($file, array('new-page-blank-template.html', 'editor.html'))) continue; //skip template files
+	$pathInfo = pathinfo($file);
+	$filename = $pathInfo['filename'];
+	$folder = preg_replace('@/.+?$@', '', $pathInfo['dirname']);
+	$subfolder = preg_replace('@^.+?/@', '', $pathInfo['dirname']);
+	if ($filename == 'index' && $subfolder) {
+		$filename = $subfolder;
+	}
+	$url = $pathInfo['dirname'] . '/' . $pathInfo['basename'];
+	$name = ucfirst($filename);
+
+	$files .= "{name:'$name', file:'$filename', title:'$name',  url: '$url', folder:'$folder'},";
+}
 
 
 //replace files list from html with the dynamic list from demo folder
